7025b11db0fbd1c5ebf8cc01511a9fd09b387728
bvqqon <abdralibaqdaulet@gmail.com>
2025-11-19 23:02:24 +0500

    Remove Mumbai network configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,5 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    mumbai: {
-      url: process.env.MUMBAI_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
   }
 };
